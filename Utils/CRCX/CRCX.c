a9d04345bab057691240553c24676f8f0d2eb762
Tim Lamborn <tlamborn@gmail.com>
2025-11-19 11:47:44 +1300

    Fix CRCX size_t ref

--- Utils/CRCX/CRCX.c
+++ Utils/CRCX/CRCX.c
@@ -55,7 +55,7 @@ uint8_t CRC8R(uint8_t init, uint8_t poly, const uint8_t * data, uint32_t size)
       return crc;
 }
 
-uint16_t CRC16(uint16_t init, uint16_t poly, const uint8_t * data, size_t size)
+uint16_t CRC16(uint16_t init, uint16_t poly, const uint8_t * data, uint32_t size)
 {
 	uint16_t crc = init;
 	while (size--)
@@ -72,7 +72,7 @@ uint16_t CRC16(uint16_t init, uint16_t poly, const uint8_t * data, size_t size)
 	return crc;
 }
 
-uint16_t CRC16R(uint16_t init, uint16_t poly, const uint8_t * data, size_t size)
+uint16_t CRC16R(uint16_t init, uint16_t poly, const uint8_t * data, uint32_t size)
 {
 	uint16_t crc = init;
 	while (size--)
@@ -90,7 +90,7 @@ uint16_t CRC16R(uint16_t init, uint16_t poly, const uint8_t * data, size_t size)
 }
 
 
-uint16_t CRC16_CCITT(uint16_t init, const uint8_t * data, size_t size)
+uint16_t CRC16_CCITT(uint16_t init, const uint8_t * data, uint32_t size)
 {
 	uint16_t crc = init;
 	while (size--)
